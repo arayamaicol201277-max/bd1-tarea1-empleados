dee23f917614657b0966e7f27214d81c7b0d35ca
Jean <Jean@COMPU_JEAN>
2026-09-01 20:17:38 -0600

    Add project files.

---
+++ Registro de Empleados/Pages/Empleados.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Registro_de_Empleados.Pages
+{
+    public class EmpleadosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Registro de Empleados/Pages/Models/Empleado.cs
@@ -0,0 +1,11 @@
+﻿namespace Registro_de_Empleados.Pages.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Salario { get; set; }
+
+    }
+
+}
