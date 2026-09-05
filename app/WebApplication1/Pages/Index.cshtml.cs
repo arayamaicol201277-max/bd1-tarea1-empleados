4a6009ff254297e17ed0b93fdf02ca5b7f99a81e
edwin <arayamaicol201277@gmail.com>
2026-09-05 11:29:01 -0600

    web rudimentaria

--- app/WebApplication1/Pages/Index.cshtml.cs
+++ app/WebApplication1/Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebApplication.Pages
+namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
--- app/WebApplication1/Program.cs
+++ app/WebApplication1/Program.cs
@@ -1,8 +1,10 @@
+using WebApplication1.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddScoped<EmpleadoService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
