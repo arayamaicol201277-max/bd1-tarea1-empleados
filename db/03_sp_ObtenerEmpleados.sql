21ee1316b5a072485fdaf8fa65cf0afdd0cf804d
edwin <arayamaicol201277@gmail.com>
2026-09-07 23:50:38 -0600

    Actualización de los sp´s y el docs

--- db/03_sp_ObtenerEmpleados.sql
+++ db/03_sp_ObtenerEmpleados.sql
@@ -1,5 +1,5 @@
 IF OBJECT_ID('dbo.sp_ObtenerEmpleados', 'P') IS NOT NULL
-    DROP PROCEDURE dbo.sp_ObtenerEmpleados;
+    DROP PROCEDURE dbo.sp_ObtenerEmpleados; -- Elimina el sp si ya existe.
 GO
 
 CREATE PROCEDURE dbo.sp_ObtenerEmpleados
@@ -7,15 +7,18 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
+    -- Se usa alias "e" para la tabla Empleado, siguiendo el
+    -- estándar de nombrar tablas mediante alias en el SELECT.
     SELECT
-        id,
-        Nombre,
-        Salario
-    FROM dbo.Empleado
-    ORDER BY Nombre ASC;
+        e.id,
+        e.Nombre,
+        e.Salario
+    FROM dbo.Empleado AS e
+    ORDER BY e.Nombre ASC;
+
+    SET NOCOUNT OFF;
 END
 GO
-
 
 -- Prueba manual:
 -- EXEC dbo.sp_ObtenerEmpleados;
--- db/04_sp_InsertarEmpleado.sql
+++ db/04_sp_InsertarEmpleado.sql
@@ -1,36 +1,40 @@
 IF OBJECT_ID('dbo.sp_InsertarEmpleado', 'P') IS NOT NULL
-    DROP PROCEDURE dbo.sp_InsertarEmpleado;
+    DROP PROCEDURE dbo.sp_InsertarEmpleado; -- Elimina el sp si ya existe.
 GO
 
 CREATE PROCEDURE dbo.sp_InsertarEmpleado
     @Nombre   VARCHAR(128),
     @Salario  MONEY,
-    @Codigo   INT           OUTPUT,  
+    @Codigo   INT           OUTPUT,
     @Mensaje  VARCHAR(200)  OUTPUT
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    
+    -- Validación programática de duplicado: se usa alias "e"
+    -- para la tabla Empleado, consistente con el resto de
+    -- los procedimientos del proyecto.
     IF EXISTS (
         SELECT 1
-        FROM dbo.Empleado
-        WHERE Nombre = @Nombre
+        FROM dbo.Empleado AS e
+        WHERE e.Nombre = @Nombre
     )
     BEGIN
         SET @Codigo  = 1;
         SET @Mensaje = 'Nombre de Empleado ya existe.';
+        SET NOCOUNT OFF;
         RETURN;
     END
 
+    -- Si no existe duplicado, se procede con la inserción.
     INSERT INTO dbo.Empleado (Nombre, Salario)
     VALUES (@Nombre, @Salario);
 
     SET @Codigo  = 0;
     SET @Mensaje = 'Inserción exitosa.';
+    SET NOCOUNT OFF;
 END
 GO
-
 
 -- Prueba manual:
 -- DECLARE @Codigo INT, @Mensaje VARCHAR(200);
